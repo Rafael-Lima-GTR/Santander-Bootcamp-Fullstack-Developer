a4702f54a93b8e750644404a246683fb8c88a266
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-06-20 19:35:57 -0300

    Fiz o exercício prático "Criando um Contador" proposto pela @stebsnusch no Curso de Introdução ao JavaScript.

---
+++ Exemplos_das_Aulas/Módulo_II-HTML_CSS_JS/03-Int._ao_JavaScript/assets/js/script.js
@@ -0,0 +1,23 @@
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+    if(count >= 0){
+        CURRENT_NUMBER.style.color = 'white';
+    }
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+    if(count < 0){
+        CURRENT_NUMBER.style.color = 'red';
+    }
+}
+
+function test() {
+	kdowkdpo;
+}
